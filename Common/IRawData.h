2e6c3cac19fc5987e9755dd7df4a15bbc0afa7f7
Amrik Sadhra <AmrikSadhra@gmail.com>
2025-02-18 21:30:38 +0000

    CI Fix for MSVC (bswap)

--- Common/IRawData.h
+++ Common/IRawData.h
@@ -9,6 +9,15 @@
 #include "NFSVersion.h"
 #include "Utils.h"
 
+#ifdef _MSC_VER
+#include <stdlib.h>
+#define bswap_16(x) _byteswap_ushort(x)
+#define bswap_32(x) _byteswap_ulong(x)
+#else
+#define bswap_16(x) __builtin_bswap16(x)
+#define bswap_32(x) __builtin_bswap32(x)
+#endif
+
 template <typename T>
 [[nodiscard]] static bool safe_read(std::ifstream &ifstream, T &structure, size_t const size = sizeof(T)) {
     return ifstream.read((char *)&structure, (std::streamsize)size).gcount() == size;
@@ -23,9 +32,9 @@ template <typename T, typename = std::enable_if_t<std::is_same_v<T, uint16_t> ||
 [[nodiscard]] static bool safe_read_bswap(std::ifstream &ifstream, T &x) {
     bool const success{safe_read(ifstream, x)};
     if (std::is_same<T, uint16_t>::value) {
-        x = __builtin_bswap16(x);
+        x = bswap_16(x);
     } else {
-        x = __builtin_bswap32(x);
+        x = bswap_32(x);
     }
     return success;
 }
@@ -36,9 +45,9 @@ template <typename T, size_t N,
     bool const success{safe_read(ifstream, arr, N * sizeof(T))};
     for (auto &elem : arr) {
         if (std::is_same<T, uint16_t>::value) {
-            elem = __builtin_bswap16(elem);
+            elem = bswap_16(elem);
         } else {
-            elem = __builtin_bswap32(elem);
+            elem = bswap_32(elem);
         }
     }
     return success;
@@ -51,9 +60,9 @@ template <typename T> static void write(std::ofstream &ofstream, T &x) {
 template <typename T, typename = std::enable_if_t<std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t>>>
 static void write_bswap(std::ofstream &ofstream, T &x) {
     if (std::is_same<T, uint16_t>::value) {
-        x = __builtin_bswap16(x);
+        x = bswap_16(x);
     } else {
-        x = __builtin_bswap32(x);
+        x = bswap_32(x);
     }
     ofstream.write((char *)&x, (std::streamsize)sizeof(T));
 }
@@ -63,9 +72,9 @@ template <typename T, size_t N,
 static void write_bswap(std::ofstream &ofstream, std::array<T, N> &arr) {
     for (auto &elem : arr) {
         if (std::is_same<T, uint16_t>::value) {
-            elem = __builtin_bswap16(elem);
+            elem = bswap_16(elem);
         } else {
-            elem = __builtin_bswap32(elem);
+            elem = bswap_32(elem);
         }
     }
     ofstream.write((char *)arr.data(), (std::streamsize)N * sizeof(T));
